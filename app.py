7b5e195099c69d147618a18cab71e2f74b0cc817
Glandy Mundung <61392816+GlandyMundung@users.noreply.github.com>
2022-11-29 18:49:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -118,7 +118,7 @@ def success():
     else:
         return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(debug = True)
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000, debug=True)
 
 
